54d00674a3fd4771ee18ee9e428f8a98ba9dad21
Shubail Haque Turzo <trzhaque2@gmail.com>
2023-08-30 00:25:41 +0600

    ok

---
+++ B_Far_Relative_s_Problem.cpp
@@ -0,0 +1,64 @@
+/******************CODE*********************/
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<string> vstr;
+typedef vector<long long> vll;
+#define pb push_back
+#define ll long long
+#define all(v) v.begin(),v.end()
+#define acs(v, x) accumulate(all(v), x);
+#define acm(v,x) accumulate(all(v), 1,multiplies<ll>());
+#define ers(num) num.erase(num.begin()) // removing the array's first element
+#define asc(v) sort(all(v))
+#define dsc(v) sort(v.begin(), v.end(), greater<int>())
+#define rev(v) reverse(all(v))
+#define unique_resize(v) v.resize(distance(v.begin(), unique(all(v))));
+#define Fast_ios ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+//Shortcut
+#define dp(x) cout << x << endl
+#define dp2(x,y) cout << x << " " << y << endl
+void print(){cout<<endl;}template<typename T,typename... A>void print(const T& a,const A&... z){cout<<a<<' ';print(z...);}
+void input(){}template<typename T,typename... A>void input(T& a,A&... z){cin>>a;input(z...);}
+template<typename T>void print(vector<T>& v){for(auto& i:v)cout<<i<<' ';cout<<endl;}
+template<typename T,typename S>void print(vector<T>& v,S s){for(auto& i:v)cout<<i<<s;}
+template<typename T>void input(vector<T> &v){for(auto& i:v)cin>>i;}
+
+
+// Solution
+void solve()
+{
+       ll n;cin>>n;
+       vll m(367,0),f(367,0);
+    while(n--){
+        string s;cin>>s;
+        ll i,j;cin>>i>>j;
+        if(s=="M"){
+            for(i;i<=j;i++){
+                m[i]++;
+            }
+        }else {
+            for(i;i<=j;i++){
+                f[i]++;
+            }
+        }
+    }
+    ll mx=0,mi=INT_MAX;
+    for(ll i=0;i<367;i++){
+        mi=min(m[i],f[i]);
+        mx=max(mx,2*mi);
+    }
+    dp(mx);
+}
+
+signed main()
+{
+    Fast_ios;
+    int t = 1;
+    //cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
